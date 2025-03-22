8916aad47c9cd5e18288c7573683762891da6791
Shehab Ahmed <shehabeldeenahmed727@gmail.com>
2025-03-22 14:20:53 +0200

    tf state file updated

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "my-tfstatefile-shehab" 
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
